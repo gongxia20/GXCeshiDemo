2f38689832081fa258e7f35ddbc1d94bf2c7710e
James <gongxia20@sina.cn>
2018-09-16 00:58:03 +0800

    初始化项目

---
+++ GXVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GXVideo
+//
+//  Created by James on 2018/9/16.
+//  Copyright © 2018年 TZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GXVideo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GXVideo
+//
+//  Created by James on 2018/9/16.
+//  Copyright © 2018年 TZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
